c3216f568efad79d68e2b369e26fdffb10f38b5a
Stadnik Raphael <raphael.stadnik@ipmgroup.be>
2026-03-27 11:05:13 +0100

    9.React-Redux : Utiliser un custom middleware

--- 9.React-Redux/src/store.js
+++ 9.React-Redux/src/store.js
@@ -9,4 +9,27 @@ export const store = configureStore({
     fruits,
     fruitCart: fruitsCACA,
   },
+  // Middlewares = mécanisme qui permet d'intercepter les actions et qui va permettre d'effectuer certaines actions secondaires.
+  // Attention next() va vraiment bloquer les autres actions et effectuer l'action du middleware.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(customMiddleware),
 });
+
+function customMiddleware(store) {
+  return function (next) {
+    return function (action) {
+      // console.log("Middleware", store.getState());
+      // console.log("Middleware", store.dispatch());
+      // retourne l'action vers le store. Sinon le middleware stoppe l'action.
+      next({
+        type: "fruitsCart/add",
+        payload: {
+          name: "Mango",
+          url: "/images/mango.jpg",
+          price: 999,
+          id: 66555665,
+        },
+      });
+    };
+  };
+}
