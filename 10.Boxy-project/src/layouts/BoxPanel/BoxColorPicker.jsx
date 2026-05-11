bf6f269a0f5bebdc4385a560eae3e4a7941a2bf0
Stadnik Raphael <raphael.stadnik@ipmgroup.be>
2026-05-11 17:22:21 +0200

    10.Boxy-project: Mettre en place l'input color lié à la boîte

--- 10.Boxy-project/src/layouts/BoxPanel/BoxColorPicker.jsx
+++ 10.Boxy-project/src/layouts/BoxPanel/BoxColorPicker.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { updateBoxValue } from "../../features/boxProperties";
 
-export default function BoxColorPicker() {
-  return <div>BoxColorPicker</div>;
+export default function BoxColorPicker({ inputData }) {
+  const dispatch = useDispatch();
+
+  function handleInputs(e) {
+    dispatch(
+      updateBoxValue({
+        inputNumber: inputData.inputNumber,
+        value: e.target.value,
+      }),
+    );
+  }
+  return (
+    <div className="mt-3">
+      <p>{inputData.name}</p>
+      <div className="flex mt-2">
+        <input
+          type="text"
+          onChange={handleInputs}
+          value={inputData.value}
+          className="flex-grow border py-1 px-2 focus:outline-1 outline-gray-400"
+        />
+        <input
+          type="color"
+          onChange={handleInputs}
+          value={inputData.value}
+          className="cursor-pointer h-[40px]"
+        />
+      </div>
+    </div>
+  );
 }
