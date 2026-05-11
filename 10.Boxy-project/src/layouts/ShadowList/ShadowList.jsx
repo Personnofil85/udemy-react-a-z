3c3dff363a7d4b5dbf66c581f67cff4af1f16396
Stadnik Raphael <raphael.stadnik@ipmgroup.be>
2026-05-11 17:40:11 +0200

    10.Boxy-project: Afficher la liste des ombres

--- 10.Boxy-project/src/layouts/ShadowList/ShadowList.jsx
+++ 10.Boxy-project/src/layouts/ShadowList/ShadowList.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addShadow } from "../../features/shadows";
+import Shadow from "./Shadow.jsx";
 
 export default function ShadowList() {
-  return <div>ShadowList</div>;
+  const shadows = useSelector((state) => state.shadows);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div className="flex justify-between p-6 border-b border-gray-300">
+        <p className="font-bold text-lg">Customize Shadows</p>
+        <button
+          onClick={() => dispatch(addShadow())}
+          className="py-1 px-3 text-sm rounded bg-blue-600 focus:outline-none focus:ring-4 focus:ring-offset-2 hover:bg-blue-700 text-white"
+        >
+          Add Shadow
+        </button>
+      </div>
+      <ul>
+        {shadows.map((shadow, index) => (
+          <Shadow key={shadow.id} shadow={shadow} panelNumber={index + 1} />
+        ))}
+      </ul>
+    </div>
+  );
 }
