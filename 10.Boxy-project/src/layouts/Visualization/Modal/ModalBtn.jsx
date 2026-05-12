132932d82ef55c9a4b106d074e9217e80dbb7fc6
Stadnik Raphael <raphael.stadnik@ipmgroup.be>
2026-05-12 17:44:37 +0200

    10.Boxy-project: Ajouter la visualisation

---
+++ 10.Boxy-project/src/layouts/Visualization/Modal/ModalBtn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ModalBtn() {
+  return (
+    <div className="cursor-pointer mx-auto mt-2 py-1 px-3 text-sm rounded bg-blue-600 text-white hover:bg-blue-700">
+      Get the code
+    </div>
+  );
+}
---
+++ 10.Boxy-project/src/layouts/Visualization/Modal/ModalResult.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ModalResult() {
+  return <div>ModalResult</div>;
+}
