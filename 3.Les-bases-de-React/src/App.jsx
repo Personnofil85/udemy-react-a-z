0c1b60bac7a08e29e9b318f008340c5ffc60db92
Stadnik Raphael <raphael.stadnik@ipmgroup.be>
2026-06-22 10:42:21 +0200

    3.Les-bases-de-React: Mise en place

---
+++ 3.Les-bases-de-React/src/App.jsx
@@ -0,0 +1,13 @@
+// import Container from "./lessons/1.Le-JSX/Container.jsx";
+import Container from "./lessons/2.Premier-composant/Container.jsx";
+
+function App() {
+  return (
+    <div className="main-content">
+      <h1>App composant racine</h1>
+      <Container />
+    </div>
+  );
+}
+
+export default App;
