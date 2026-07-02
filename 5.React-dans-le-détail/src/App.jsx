933325b136edb484356df7d6d9433620a09290ff
Stadnik Raphael <raphael.stadnik@ipmgroup.be>
2026-07-02 15:24:03 +0200

    5.React-detail: Mettre en place les leçons

---
+++ 5.React-dans-le-détail/src/App.jsx
@@ -0,0 +1,14 @@
+import Container from "./lessons/1.Les-fragments/Container.jsx";
+import Container2 from "./lessons/2.La-règle-des-hooks/Container.jsx";
+
+function App() {
+  return (
+    <div className="main-content">
+      <Container />
+      <hr />
+      <Container2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ 5.React-dans-le-détail/src/lessons/1.Les-fragments/Container.jsx
@@ -0,0 +1,10 @@
+export default function Container() {
+  return (
+    <>
+      <h1>Les fragments</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  );
+}
